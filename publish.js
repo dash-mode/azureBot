cc427e6af47dd81580ef3f8271cce099501960c4
Dibyadarshi <ddash1999@gmail.com>
2019-06-16 11:00:30 -0400

    first commit

---
+++ publish.js
@@ -0,0 +1,52 @@
+var zipFolder = require('zip-folder');
+var path = require('path');
+var fs = require('fs');
+var request = require('request');
+
+var rootFolder = path.resolve('.');
+var zipPath = path.resolve(rootFolder, '../schecter.zip');
+var kuduApi = 'https://schecter.scm.azurewebsites.net/api/zip/site/wwwroot';
+var userName = '$schecter';
+var password = 'gH3hKE84charRC5BdcogjonLxiPSNwNaekjrTqAvTKK05xymH5nXpkGhpwwj';
+
+function uploadZip(callback) {
+  fs.createReadStream(zipPath).pipe(request.put(kuduApi, {
+    auth: {
+      username: userName,
+      password: password,
+      sendImmediately: true
+    },
+    headers: {
+      "Content-Type": "applicaton/zip"
+    }
+  }))
+  .on('response', function(resp){
+    if (resp.statusCode >= 200 && resp.statusCode < 300) {
+      fs.unlink(zipPath);
+      callback(null);
+    } else if (resp.statusCode >= 400) {
+      callback(resp);
+    }
+  })
+  .on('error', function(err) {
+    callback(err)
+  });
+}
+
+function publish(callback) {
+  zipFolder(rootFolder, zipPath, function(err) {
+    if (!err) {
+      uploadZip(callback);
+    } else {
+      callback(err);
+    }
+  })
+}
+
+publish(function(err) {
+  if (!err) {
+    console.log('schecter publish');
+  } else {
+    console.error('failed to publish schecter', err);
+  }
+});
